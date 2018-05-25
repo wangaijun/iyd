2dd31767b944cd7409daac89cbe003e267465d22
waj <wangaj@glodon.com>
2018-05-25 08:20:51 +0800

    重命名

--- src/model/Word.kt
+++ src/model/Word.kt
@@ -1,7 +1,7 @@
 package model
 
-data class Word(val q:String, val r:String, val usp:String, val ukp:String, var reviewTime: Long, var reviewCount:Int){
+data class Word(val q:String, val r:String, val usp:String, val ukp:String, var reviewTime: Long, var reviewedCount:Int){
     override fun toString(): String {
-        return "$q\n$r\n$usp\n$ukp\n$reviewTime\n$reviewCount"
+        return "$q\n$r\n$usp\n$ukp\n$reviewTime\n$reviewedCount"
     }
 }
