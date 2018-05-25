d8d3d01a51001449336c76777f6049c574c77a11
waj <wangaj@glodon.com>
2018-05-25 08:13:04 +0800

    每个项占一行

--- src/model/Word.kt
+++ src/model/Word.kt
@@ -1,3 +1,7 @@
 package model
 
-data class Word(val q:String, val r:String, val usp:String, val ukp:String){}
+data class Word(val q:String, val r:String, val usp:String, val ukp:String){
+    override fun toString(): String {
+        return "$q\n$r\n$usp\n$ukp)"
+    }
+}
