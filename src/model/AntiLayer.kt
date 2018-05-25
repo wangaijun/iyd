8c67a8c7867b5b0128c3cd0f3a9991e3be721e57
waj <wangaj@glodon.com>
2018-05-25 08:19:58 +0800

    增加下次复习时间和已复习次数

--- src/model/AntiLayer.kt
+++ src/model/AntiLayer.kt
@@ -2,6 +2,7 @@ package model
 
 object AntiLayer{
     fun toWord(q:String,basicBean: BasicBean):Word{
-        return Word(q,basicBean.explains.toString(),basicBean.usphonetic?:"",basicBean.ukphonetic?:"")
+        return Word(q,basicBean.explains.toString(),basicBean.usphonetic?:"",basicBean.ukphonetic?:""
+        ,System.currentTimeMillis()+8*60*60*1000,0)
     }
 }
--- src/model/Word.kt
+++ src/model/Word.kt
@@ -1,7 +1,7 @@
 package model
 
-data class Word(val q:String, val r:String, val usp:String, val ukp:String){
+data class Word(val q:String, val r:String, val usp:String, val ukp:String, var reviewTime: Long, var reviewCount:Int){
     override fun toString(): String {
-        return "$q\n$r\n$usp\n$ukp)"
+        return "$q\n$r\n$usp\n$ukp\n$reviewTime\n$reviewCount"
     }
 }
