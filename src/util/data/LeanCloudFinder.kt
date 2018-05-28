7bfa2caa25d0854da28678dd83d8edc07d6e49af
waj <wangaj@glodon.com>
2018-05-28 08:41:47 +0800

    获取生词

--- src/util/data/LeanCloudFinder.kt
+++ src/util/data/LeanCloudFinder.kt
@@ -6,10 +6,12 @@ import com.google.gson.JsonParser
 import com.google.gson.reflect.TypeToken
 import model.Word
 import util.getRequest
+import java.util.*
 
 class LeanCloudFinder: Finder(){
     override fun find(): ArrayList<Word> {
         val map = hashMapOf<String,String>()
+//        map["where"] = "reviewTime<" + (System.currentTimeMillis()-24*60*60*1000).toString()
         val s = getRequest("https://leancloud.cn:443/1.1/classes/word",map)
         val root = JsonParser().parse(s)
         root as JsonObject
