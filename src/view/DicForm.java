d8bcd9a6cae4b96f4bdecfde439809422ae64c64
waj <wangaj@glodon.com>
2018-05-28 14:11:22 +0800

    增加词典GUI

--- src/view/DicForm.java
+++ src/view/DicForm.java
@@ -3,19 +3,20 @@ package view;
 import javax.swing.*;
 
 public class DicForm {
+    private JPanel panel;
+    private JTextField textField1;
     private JButton button1;
-    private JPanel panel1;
     Alert alert = new Alert();
 
     public DicForm() {
-        button1.addActionListener(e -> {
-            alert.build("hello,world!");
-        });
+//        button1.addActionListener(e -> {
+//            alert.build("hello,world!");
+//        });
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("DicForm");
-        frame.setContentPane(new DicForm().panel1);
+        frame.setContentPane(new DicForm().panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
