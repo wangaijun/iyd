7a1f385549750ff15cc281b6f3e9ff0b5a2a9c56
waj <wangaj@glodon.com>
2018-05-28 14:59:59 +0800

    修改界面字体大小

--- src/view/DicForm.java
+++ src/view/DicForm.java
@@ -1,17 +1,48 @@
 package view;
 
+import applayer.WordsNote;
+import model.AntiLayer;
+import model.BasicBean;
+import util.AudioPlayer;
+
 import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static applayer.DictionaryKt.lookAndPlay;
 
 public class DicForm {
     private JPanel panel;
     private JTextField textField1;
+    private JTextArea textArea1;
     private JButton button1;
+    private JButton button2;
     Alert alert = new Alert();
+    private String q;
+    private BasicBean basicBean;
 
     public DicForm() {
-//        button1.addActionListener(e -> {
-//            alert.build("hello,world!");
-//        });
+        setViewProperty();
+        setViewListener();
+    }
+
+    private void setViewProperty() {
+        textField1.setFont(new Font("宋体",Font.BOLD,20));
+        textArea1.setFont(new Font("宋体",Font.BOLD,16));
+    }
+
+    private void setViewListener() {
+        textField1.addActionListener(e -> {
+            q = textField1.getText();
+            basicBean = lookAndPlay(new String[]{q});
+            textArea1.setText(basicBean.toString());
+        });
+        button1.addActionListener(e -> new AudioPlayer().play(q));
+        button2.addActionListener(e -> {
+            if (basicBean!=null) WordsNote.INSTANCE.save(AntiLayer.INSTANCE.toWord(q,basicBean));
+        });
     }
 
     public static void main(String[] args) {
